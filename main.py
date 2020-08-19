b63c20deac67a8131ae1e5ea63a452b92ffc6283
AthenaBC <biancaytchan@gmail.com>
2020-08-19 00:32:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Welcome to Bianca's Fabric Shop. What would you like?")
+print("A - Blue Fabric. B - Pink Fabric  C - Red Fabric")
+userInput=input()
+
+if userInput == "A":
+  print("A Blue Fabric Made of Silk")
+elif userInput == "B":
+  print("A Pink Fabric Made of Cotton")
+else:
+  print("A Red Fabric Made of Wool")
